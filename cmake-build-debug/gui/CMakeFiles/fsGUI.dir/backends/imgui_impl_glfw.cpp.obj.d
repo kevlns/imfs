da6ab68ba766c3fa2d0547d6f95d7707ceda50d7
SL <58720287+Kev-Long@users.noreply.github.com>
2023-05-05 18:57:07 +0800

    Delete cmake-build-debug directory

--- cmake-build-debug/gui/CMakeFiles/fsGUI.dir/backends/imgui_impl_glfw.cpp.obj.d
+++
@@ -1,4 +0,0 @@
-C:/Program Files (x86)/Windows Kits/10/include/10.0.22000.0/um/jobapi2.h
-C:/Program Files (x86)/Windows Kits/10/include/10.0.22000.0/shared/pshpack8.h
-C:/Program Files (x86)/Windows Kits/10/include/10.0.22000.0/um/objbase.h
-C:/Program Files (x86)/Windows Kits/10/include/10.0.22000.0/um/oleauto.h
--- cmake-build-debug/gui/CMakeFiles/fsGUI.dir/imgui_tables.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-D:/Microsoft.apps/Microsoft Visual Studio/2022/Professional/VC/Tools/MSVC/14.35.32215/include/nmmintrin.h
-D:/Microsoft.apps/Microsoft Visual Studio/2022/Professional/VC/Tools/MSVC/14.35.32215/include/smmintrin.h
-D:/Microsoft.apps/Microsoft Visual Studio/2022/Professional/VC/Tools/MSVC/14.35.32215/include/tmmintrin.h
